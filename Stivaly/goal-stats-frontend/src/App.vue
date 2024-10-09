6fe8f25626aedfa8198109d3847aa76f75a8f791
Stivaly <Stivaly.g@hotmail.com>
2024-10-08 21:09:47 -0300

    1st commit
    Creacion de proyecto vue, configuracion de router

---
+++ Stivaly/goal-stats-frontend/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <nav v-if="showNav">
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/register">Register</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    showNav() {
+      const routesWithNav = ['/dashboard',]; // Lista de rutas donde se muestra el menú
+      return routesWithNav.includes(this.$route.path); 
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ Stivaly/goal-stats-frontend/src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import InicioSesion from '@/views/UserLogin.vue'
+import Register from '@/views/UserRegister.vue'
+
+const routes = [
+  {
+    path: '/login',
+    name: 'Login',
+    component: InicioSesion
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
