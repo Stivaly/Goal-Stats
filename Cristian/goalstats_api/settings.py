5a396428d710b62e340392a3ae3fa3c78c1f73f2
Cristian Andres Valenzuela <tc.cvalenzuela@gmail.com>
2024-10-10 21:17:21 -0300

    cambio archivos

--- Cristian/goalstats_api/settings.py
+++ Cristian/goalstats_api/settings.py
@@ -154,8 +154,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS Fixed
 CORS_ORIGIN_ALLOW_ALL = True
 CSRF_TRUSTED_ORIGINS = [
-        'http://127.0.0.1:3000',
+        'http://localhost:3000',
     ]
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:3000',
+    'http://localhost:3000',
 ]
